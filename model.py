c6d5ee23effcc7febdcc0fb252e45258a22392da
User <saharirem03@gmail.com>
2025-06-25 13:42:23 +0300

    Train model in Model Class

--- model.py
+++ model.py
@@ -1,3 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO("yolo11n.pt")
+class Model:
+    def __init__(self,model = YOLO("yolo11n.pt") ):
+        self.model = model
+    def detect(self,frame):
+        results = self.model(frame)
+        annotated_frame = results[0].plot()
+        return annotated_frame
+
